1e94b99644ac381a14420b12cc9fd2a5b0184bc4
Simon Tharby <jinjamail@gmail.com>
2020-06-26 15:44:43 +0100

    playing with plug implementation

--- hello/lib/hello_web/controllers/hello_controller.ex
+++ hello/lib/hello_web/controllers/hello_controller.ex
@@ -1,11 +1,19 @@
 defmodule HelloWeb.HelloController do
   use HelloWeb, :controller
 
+  plug :bibble
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
   def show(conn, %{"messenger" => messenger}) do
     render(conn, "show.html", messenger: messenger)
+  end
+
+  def bibble(conn, _) do
+    IO.puts "bobble\n"
+
+    conn
   end
 end
