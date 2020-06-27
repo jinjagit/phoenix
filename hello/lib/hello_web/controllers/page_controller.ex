700c1060e11b91f45c2270fb2c5ffb4fdf679b79
Simon Tharby <jinjamail@gmail.com>
2020-06-27 15:38:54 +0100

    playing with views

--- hello/lib/hello_web/controllers/page_controller.ex
+++ hello/lib/hello_web/controllers/page_controller.ex
@@ -2,10 +2,8 @@ defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
-    #render(conn, :index)
-    conn
-    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
-    |> put_flash(:error, "Let's pretend we have an error.")
-    |> render("index.html")
+    render(conn, :index)
   end
+
+
 end
--- hello/lib/hello_web/views/page_view.ex
+++ hello/lib/hello_web/views/page_view.ex
@@ -1,3 +1,11 @@
 defmodule HelloWeb.PageView do
   use HelloWeb, :view
+
+#  def render(name = "index.html", assigns) do
+#    "template: #{name}, rendering with assigns #{inspect Map.keys(assigns)}\n"
+#  end
+
+
+
+
 end
